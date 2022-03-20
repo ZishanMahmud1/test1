d1191ded225a75e49f058a935ff191b0ab016c78
unknown <mahmudyea@gmail.com>
2022-03-21 01:27:19 +0600

    first commit in feature_1

---
+++ appt_input.php
@@ -0,0 +1,20 @@
+<h1>Make Appointment</h1>
+
+
+
+<form method=get action=appt.php>
+
+	phone-number <input type=number name=phonenumber> <br>
+
+	<p>
+
+	 name <input type=text name=name> <br>
+     
+	<p>
+
+	  date <input type=text name=date> <br>
+	  doctor's name <input type=text name=doctor> <br>
+     
+	<input type=submit value=Insert>
+
+</form>
